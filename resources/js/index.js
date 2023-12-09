9e456f9a6e2b50892c8e185a5a7e74bb5e5b925b
Johan Löfquist <johan.lofquist@gmail.com>
2023-12-09 09:58:21 +0100

    added sound

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,16 +1,15 @@
-let mosquito = document.querySelector("#mosquito")
-let x = 200
-let y = 500
+let mosquito = document.querySelector("#mosquito");
+let x = 200;
+let y = 500;
 
 setInterval(fly, 5);
 
-
 function fly() {
-    let randomX = Math.floor(Math.random() * 5) - 2
-    let randomY = Math.floor(Math.random() * 5) - 2
-    x += randomX
-    y += randomY
-    mosquito.style.top = y + "px" 
-    mosquito.style.right = x + "px" 
-    console.log(randomX)
+  let randomX = Math.floor(Math.random() * 5) - 2;
+  let randomY = Math.floor(Math.random() * 5) - 2;
+  x += randomX;
+  y += randomY;
+  mosquito.style.top = y + "px";
+  mosquito.style.right = x + "px";
+  console.log(randomX);
 }
