69d523dad465809b86f845ee12e179af443eef36
Johan Löfquist <johan.lofquist@gmail.com>
2023-12-09 23:21:40 +0100

    random styling

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,6 +1,7 @@
 let mosquito = document.querySelector("#mosquito");
 let x = 200;
 let y = 500;
+let size = 50
 
 let interval = setInterval(fly, 5);
 
@@ -11,7 +12,7 @@ function fly() {
   y += randomY;
   mosquito.style.top = y + "px";
   mosquito.style.right = x + "px";
-  console.log(randomX);
+  console.log(randomSize);
 }
 
 mosquito.addEventListener("click", function(){
