a84e1dc438b2d52b25fddc07d8ae0b688f2e7084
Johan Löfquist <johan.lofquist@gmail.com>
2023-12-09 09:18:24 +0100

    added irritating mosquito

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,12 +2,12 @@ let mosquito = document.querySelector("#mosquito")
 let x = 200
 let y = 500
 
-setInterval(fly, 30);
+setInterval(fly, 5);
 
 
 function fly() {
-    let randomX = Math.floor(Math.random() * 9) - 4
-    let randomY = Math.floor(Math.random() * 9) - 4
+    let randomX = Math.floor(Math.random() * 5) - 2
+    let randomY = Math.floor(Math.random() * 5) - 2
     x += randomX
     y += randomY
     mosquito.style.top = y + "px" 
