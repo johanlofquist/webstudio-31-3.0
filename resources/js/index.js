6f17cf3f53b0e9b18b77153ab28a76adfc6fbbfd
Johan Löfquist <johan.lofquist@gmail.com>
2023-12-10 22:57:22 +0100

    Finalizing

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,7 +1,7 @@
 let mosquito = document.querySelector("#mosquito");
 let x = 200;
 let y = 500;
-let size = 50
+let size = 50;
 
 let interval = setInterval(fly, 5);
 
@@ -14,32 +14,101 @@ function fly() {
   mosquito.style.right = x + "px";
 }
 
-mosquito.addEventListener("click", function(){
-    clearInterval(interval)
-    let squish = new Audio("/resources/img/squish.mp3");
-    squish.play();
-    mosquito.src = "resources/img/deadmygga.svg"
-})
+mosquito.addEventListener("click", function () {
+  clearInterval(interval);
+  let squish = new Audio("/resources/img/squish.mp3");
+  squish.play();
+  mosquito.src = "resources/img/deadmygga.svg";
+});
 
-let burgerMenu = document.querySelector("#hamburger__menu")
-let lineOne = document.querySelector("#line__one")
-let lineTwo = document.querySelector("#line__two")
+let burgerMenu = document.querySelector("#hamburger__menu");
+let lineOne = document.querySelector("#line__one");
+let lineTwo = document.querySelector("#line__two");
+let menu = document.querySelector(".menu");
+let bodyEl = document.querySelector("body");
 
-let isToggled = false
+let isToggled = false;
 
-burgerMenu.addEventListener("click", function() {
-  if (isToggled == false) { 
-  lineOne.classList.remove("burgerSpinOneBack");
-  lineTwo.classList.remove("burgerSpinTwoBack");
-  lineOne.classList.add("burgerSpinOneToX");
-  lineTwo.classList.add("burgerSpinTwoToX");
-  isToggled = true
+burgerMenu.addEventListener("click", function () {
+  if (isToggled == false) {
+    lineOne.classList.remove("burgerSpinOneBack");
+    lineTwo.classList.remove("burgerSpinTwoBack");
+    lineOne.classList.add("burgerSpinOneToX");
+    lineTwo.classList.add("burgerSpinTwoToX");
+    menu.classList.remove("hide__menu");
+    menu.classList.add("show__menu");
+    bodyEl.style.overflow = "hidden";
+
+    isToggled = true;
   } else {
     lineOne.classList.remove("burgerSpinOneToX");
     lineTwo.classList.remove("burgerSpinTwoToX");
     lineOne.classList.add("burgerSpinOneBack");
     lineTwo.classList.add("burgerSpinTwoBack");
-    isToggled = false
+    menu.classList.remove("show__menu");
+    menu.classList.add("hide__menu");
+    bodyEl.style.overflow = "auto";
+    isToggled = false;
   }
-})
+});
 
+let menuHome = document.querySelector("#menu__hem");
+let menuCase = document.querySelector("#menu__case")
+let menuServices = document.querySelector("#menu__services")
+let menuOm = document.querySelector("#menu__om")
+let menuKontakt = document.querySelector("#menu__kontakt")
+
+menuHome.addEventListener("click", function () {
+  lineOne.classList.remove("burgerSpinOneToX");
+  lineTwo.classList.remove("burgerSpinTwoToX");
+  lineOne.classList.add("burgerSpinOneBack");
+  lineTwo.classList.add("burgerSpinTwoBack");
+  menu.classList.remove("show__menu");
+  menu.classList.add("hide__menu");
+  bodyEl.style.overflow = "auto";
+  isToggled = false;
+});
+
+menuCase.addEventListener("click", function () {
+  lineOne.classList.remove("burgerSpinOneToX");
+  lineTwo.classList.remove("burgerSpinTwoToX");
+  lineOne.classList.add("burgerSpinOneBack");
+  lineTwo.classList.add("burgerSpinTwoBack");
+  menu.classList.remove("show__menu");
+  menu.classList.add("hide__menu");
+  bodyEl.style.overflow = "auto";
+  isToggled = false;
+});
+
+menuServices.addEventListener("click", function () {
+  lineOne.classList.remove("burgerSpinOneToX");
+  lineTwo.classList.remove("burgerSpinTwoToX");
+  lineOne.classList.add("burgerSpinOneBack");
+  lineTwo.classList.add("burgerSpinTwoBack");
+  menu.classList.remove("show__menu");
+  menu.classList.add("hide__menu");
+  bodyEl.style.overflow = "auto";
+  isToggled = false;
+});
+
+menuOm.addEventListener("click", function () {
+  lineOne.classList.remove("burgerSpinOneToX");
+  lineTwo.classList.remove("burgerSpinTwoToX");
+  lineOne.classList.add("burgerSpinOneBack");
+  lineTwo.classList.add("burgerSpinTwoBack");
+  menu.classList.remove("show__menu");
+  menu.classList.add("hide__menu");
+  bodyEl.style.overflow = "auto";
+  isToggled = false;
+});
+
+menuKontakt.addEventListener("click", function () {
+  lineOne.classList.remove("burgerSpinOneToX");
+  lineTwo.classList.remove("burgerSpinTwoToX");
+  lineOne.classList.add("burgerSpinOneBack");
+  lineTwo.classList.add("burgerSpinTwoBack");
+  menu.classList.remove("show__menu");
+  menu.classList.add("hide__menu");
+  bodyEl.style.overflow = "auto";
+  isToggled = false;
+});
